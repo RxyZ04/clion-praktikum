fbd08748201d100fe02bd536b294f3ba93f80693
rajan <rajan.ahmady@smail.th-koeln.de>
2025-05-03 19:22:50 +0200

    Projekt Version v2, test mit implementierungen

--- sub.c
+++ sub.c
@@ -1,60 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include "keyValStore.h"
+#include "sub.h"
 
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 1024
 
-void handle_command(int client_socket, char* command) {
-    char key[100], value[100], response[256];
-    if (strncmp(command, "PUT", 3) == 0) {
-        sscanf(command + 4, "%s %s", key, value);
+void handle_command(int client_socket, char* buffer) {
+    char command[BUFFER_SIZE];
+    char key[MAX_KEY_LEN];
+    char value[MAX_VALUE_LEN];
+    char response[BUFFER_SIZE] = {0};
+
+    int count = sscanf(buffer, "%s %s %s", command, key, value);
+
+    if (strcmp(command, "PUT") == 0 && count == 3) {
         put(key, value);
         snprintf(response, sizeof(response), "PUT:%s:%s\n", key, value);
-    } else if (strncmp(command, "GET", 3) == 0) {
-        sscanf(command + 4, "%s", key);
-        if (get(key, value) > 0)
-            snprintf(response, sizeof(response), "GET:%s:%s\n", key, value);
+    } else if (strcmp(command, "GET") == 0 && count >= 2) {
+        char res[MAX_VALUE_LEN];
+        if (get(key, res) >= 0)
+            snprintf(response, sizeof(response), "GET:%s:%s\n", key, res);
         else
             snprintf(response, sizeof(response), "GET:%s:key_nonexistent\n", key);
-    } else if (strncmp(command, "DEL", 3) == 0) {
-        sscanf(command + 4, "%s", key);
-        if (del(key) > 0)
+    } else if (strcmp(command, "DEL") == 0 && count >= 2) {
+        if (del(key) >= 0)
             snprintf(response, sizeof(response), "DEL:%s:key_deleted\n", key);
         else
             snprintf(response, sizeof(response), "DEL:%s:key_nonexistent\n", key);
-    } else if (strncmp(command, "QUIT", 4) == 0) {
-        snprintf(response, sizeof(response), "Goodbye!\n");
+    } else if (strcmp(command, "QUIT") == 0) {
+        snprintf(response, sizeof(response), "Verbindung beendet\n");
         send(client_socket, response, strlen(response), 0);
         close(client_socket);
         exit(0);
     } else {
-        snprintf(response, sizeof(response), "Unknown command\n");
+        snprintf(response, sizeof(response), "Unbekanntes Kommando\n");
     }
+
     send(client_socket, response, strlen(response), 0);
-}
-
-void start_server(int port) {
-    int server_fd, client_socket;
-    struct sockaddr_in address;
-    char buffer[BUFFER_SIZE];
-
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-
-    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
-    listen(server_fd, 1);
-
-    socklen_t addrlen = sizeof(address);
-    client_socket = accept(server_fd, (struct sockaddr*)&address, &addrlen);
-
-    while (1) {
-        memset(buffer, 0, BUFFER_SIZE);
-        recv(client_socket, buffer, BUFFER_SIZE, 0);
-        handle_command(client_socket, buffer);
-    }
 }
--- sub.h
+++ sub.h
@@ -2,5 +2,6 @@
 #define SUB_H
 
 void start_server(int port);
+void handle_command(int client_socket, char* buffer);
 
 #endif
